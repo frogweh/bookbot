728d9dbd2de7102fe794b5ad051be89a1ab7d286
frogweh <zbernath@frogweh.org>
2025-01-18 09:50:25 -0700

    Added word count function to main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #print("hello world")
+def charCount(book):
+    return len(list(filter(None,list(book.replace("\n"," ").split(" ")))))
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_content = f.read()
-        print(type(file_content))
+        #print(type(file_content))
         #print(file_content)
+        print(charCount(file_content))
 
 main()
